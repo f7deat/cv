f3eee53a35dbc0dfb67c8de0e8af77fce61700ca
TanDC_VM <tandc_vm@novaon.dc>
2020-07-28 09:05:26 +0700

    Updates

---
+++ precache-manifest.31f252f7ce37f59cc41e696390f7b2a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dac2b37fb8993937f23e81f62c65c68",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "79948eba0e21c0e2e198",
+    "url": "/cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "095c039d018d2581864a",
+    "url": "/cv/static/js/2.16a53062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.16a53062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79948eba0e21c0e2e198",
+    "url": "/cv/static/js/main.8709ed3a.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
