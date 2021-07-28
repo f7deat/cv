82c6dcd39167c7fd18a2fd101417ade0d841cb6f
f7deat <tandc.it4@vnpt.vn>
2021-07-28 02:48:57 -0700

    blank target

--- src/components/sidebars/rights/bio.tsx
+++ src/components/sidebars/rights/bio.tsx
@@ -8,7 +8,7 @@ export default class Bio extends React.Component {
                 <br />
                 <img src="https://camo.githubusercontent.com/40dff491d4e8123af55298ef908faedb66c463e5/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f57556c706c634d704f43456d5447427442572f67697068792e676966" width="20" alt="cat coding"/> Just coding ...
                 <div className="mt-2">
-                    <a href="https://fb.me/tan.dct" className="text-primary font-weight-bolder">
+                    <a href="https://fb.me/tan.dct" className="text-primary font-weight-bolder" target="_blank">
                         +Need More
                     </a>
                 </div>
